fb702158098d09ead3e16be814d06d38438eb749
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-20 11:33:48 +0300

    Create 3_Local_Development.java

---
+++ Local_Developmnent_and _Scale/3_Local_Development.java
@@ -0,0 +1,44 @@
+Yes, you can still learn to develop scalable Rails applications locally on your machine, even without the operational cost to run all of the technologies mentioned above in a cloud like AWS.
+
+Here is a basic tech stack that you can use to run Rails applications locally:
+
+* **Ruby:** The Ruby programming language.
+* **Rails:** The Ruby on Rails framework.
+* **PostgreSQL:** A relational database management system (RDBMS).
+* **Redis:** An in-memory data store.
+* **Memcached:** Another in-memory data store.
+* **Sidekiq:** A Ruby job queue.
+
+You can install all of these technologies on your local machine using a package manager such as Homebrew or apt-get.
+
+Once you have installed all of the required technologies, you can start developing your Rails application. You can use the following command to create a new Rails application:
+
+```
+rails new my_app
+```
+
+Once the new application has been created, you can start developing it by creating controllers, models, and views.
+
+To run your Rails application locally, you can use the following command:
+
+```
+rails server
+```
+
+This will start a Rails development server on port 3000. You can then access your application in a web browser at http://localhost:3000.
+
+As you develop your Rails application, you may need to use additional technologies, such as a load balancer and a database sharding solution. However, you can start by learning how to develop Rails applications locally without using these technologies.
+
+Here are some cheap options or free options to deploy Rails applications to the cloud:
+
+* **Heroku:** Heroku is a cloud platform that offers a free tier for Rails applications.
+* **AWS Elastic Beanstalk:** AWS Elastic Beanstalk is a cloud platform that offers a free tier for Rails applications.
+* **Google Cloud Platform App Engine:** Google Cloud Platform App Engine is a cloud platform that offers a free tier for Rails applications.
+
+You can use any of these cloud platforms to deploy your Rails application for free. However, the free tiers have some limitations, such as limited resources and limited uptime.
+
+As your application grows, you may need to upgrade to a paid tier on one of these cloud platforms. However, the paid tiers are still relatively affordable.
+
+Overall, it is possible to learn to develop scalable Rails applications locally on your machine and to deploy them to the cloud at a low cost.
+
+
